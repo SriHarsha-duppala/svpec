ad1888f159039ad6b8a79b3cfc51286472093926
SriHarsha-duppala <129260957+SriHarsha-duppala@users.noreply.github.com>
2025-04-04 20:02:39 +0530

    svp.js

--- svp.js
+++ svp.js
@@ -1,9 +1,9 @@
 function appendImg(){
  const output = document.getElementById('display');
   const img = new Image();
-  img.src='./shd.jpg';
-  img.height='100'
-  img.width='100'
+  img.src='./svp8.jpg';
+  img.height='600'
+  img.width='200'
   for(let i=0;i<1;i++){
     display.appendChild(img);
   }
